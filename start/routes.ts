3a18e472e40d4569d33e9ae693c58cfa7f504e91
Cong Do <dochauthanhcong147@gmail.com>
2020-10-16 03:27:45 +0700

    feat(routes): add api prefix to router

--- start/routes.ts
+++ start/routes.ts
@@ -20,4 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.post('/login', 'AuthController.login')
+Route.group(() => {
+  Route.post('/login', 'AuthController.login')
+}).prefix('/api')
